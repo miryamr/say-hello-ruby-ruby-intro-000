0afedb9f71b593fe9b193ed6de65061856f61c00
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-02 16:33:55 +0000

    Automatically backed up by Learn

--- spec/say_hello.rb
+++ spec/say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-require_relative '../spec_helper.rb'
+require_relative 'rspec ./spec/say_hello.rb'
 def say_hello(name="Ruby programmer!")
 	puts "Hello #{name}!"
 end
